8772d44516959bab32fa386254b70d00bdd36e7b
Gordon Tucker <gordon.michael.tucker@gmail.com>
2018-08-16 15:37:18 -0600

    Created project in xcode

---
+++ FittedSheets/FittedSheets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FittedSheets
+//
+//  Created by Gordon Tucker on 8/16/18.
+//  Copyright © 2018 Gordon Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

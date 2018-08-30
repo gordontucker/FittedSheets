6bc09dee6e942c8e1802ed18ccc6f808b6885783
Gordon Tucker <gordon.michael.tucker@gmail.com>
2018-08-30 09:45:58 -0600

    Made another small change to limit the disabled scrolling to only inside the child scroll view

--- FittedSheets/Pod/SheetViewController.swift
+++ FittedSheets/Pod/SheetViewController.swift
@@ -392,7 +392,7 @@ extension SheetViewController: UIGestureRecognizerDelegate {
         let pointInChildScrollView = self.view.convert(point, to: childScrollView).y - childScrollView.contentOffset.y
         
         let velocity = panGestureRecognizer.velocity(in: panGestureRecognizer.view?.superview)
-        guard pointInChildScrollView > 0 else {
+        guard pointInChildScrollView > 0, pointInChildScrollView < childScrollView.bounds.height else {
             if keyboardHeight > 0 {
                 childScrollView.endEditing(true)
             }

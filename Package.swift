a0e8a34d8e341bfa8ed48d3a2dd948ac3c6fad73
Gordon Tucker <gordon.michael.tucker@gmail.com>
2021-01-26 10:59:58 -0700

    removed sources from spm

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .library(name: "FittedSheets", targets: ["FittedSheets"]),
     ],
     targets: [
-        .target(name: "FittedSheets", path: "FittedSheets", sources: ["FittedSheets"]),
+        .target(name: "FittedSheets", path: "FittedSheets"),
     ],
     swiftLanguageVersions: [.v5]
 )

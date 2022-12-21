f43e37cb7b57356e5964981f039547de298a42b0
Steve Landey <stevelandey@asana.com>
2022-12-21 15:15:31 -0800

    dismissOnPull obeys shouldDismiss

--- FittedSheets/SheetViewController.swift
+++ FittedSheets/SheetViewController.swift
@@ -427,7 +427,7 @@ public class SheetViewController: UIViewController {
                 
                 let animationDuration = TimeInterval(abs(velocity*0.0002) + 0.2)
                 
-                guard finalHeight > 0 || !self.dismissOnPull else {
+                guard finalHeight > 0 || !(self.dismissOnPull && self.shouldDismiss?(self) ?? true) else {
                     // Dismiss
                     UIView.animate(
                         withDuration: animationDuration,
